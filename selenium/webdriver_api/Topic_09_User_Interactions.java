7d94406ceef5fbfc02ec731de898b20da2fda951
Thuyen Nguyen <nguyenlamthuyen309@gmail.com>
2019-12-28 00:40:05 +0700

    Topic_11_Window_Tab

--- selenium/webdriver_api/Topic_09_User_Interactions.java
+++ selenium/webdriver_api/Topic_09_User_Interactions.java
@@ -1,5 +1,8 @@
 package webdriver_api;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -10,8 +13,10 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,20 +36,20 @@ public class Topic_09_User_Interactions {
 
 	@BeforeClass
 	public void beforeClass() {
-		// Tắt notification		
+		// Tắt notification
 //		  FirefoxProfile profile = new FirefoxProfile(); 
 //		  profile.setPreference("dom.webnotifications.enabled", false);
 //		  driver = new FirefoxDriver(profile);
-		 
+
 		// Capability (config browser)
 		System.setProperty("webdriver.chrome.driver", ".\\libraries\\chromedriver.exe");
-		driver = new ChromeDriver();	
+		driver = new ChromeDriver();
 		action = new Actions(driver);
 		javascriptExecutor = (JavascriptExecutor) driver;
-		
+
 		javascriptPath = ".\\dragAndDrop\\drag_anddrop_helper.js";
 		jqueryPath = ".\\dragAndDrop\\jquery_load_helper.js";
-		
+
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
@@ -52,67 +57,62 @@ public class Topic_09_User_Interactions {
 //	@Test
 	public void TC_01_() {
 		driver.get("https://www.myntra.com/");
-		
+
 		action.moveToElement(driver.findElement(By.xpath("//div[contains(@class,'navLink')]//a[text()='Men']"))).perform();
-		
+
 		driver.findElement(By.xpath("//a[@class ='desktop-categoryLink' and text()='Boxers']")).click();
-		
+
 		Assert.assertTrue(driver.findElement(By.xpath("//h1[@class='title-title' and text()='Boxers For Men']")).isDisplayed());
-		
+
 	}
 
 //	@Test
 	public void TC_02_Click_And_Hold() {
 		driver.get("https://jqueryui.com/resources/demos/selectable/display-grid.html");
-		
+
 		List<WebElement> numbers = driver.findElements(By.xpath("//ol[@id='selectable']/li"));
 		int numberSize = numbers.size();
 		System.out.println("Size before click/ hold = " + numberSize); // =12
-		
+
 		// |0 |1 |2 |3 |4 |5 |6 |...
 		// | element 01 | ...
-		
+
 		action.clickAndHold(numbers.get(0)).moveToElement(numbers.get(7)).release().perform();
-		
+
 		List<WebElement> selectedNumbers = driver.findElements(By.xpath("//ol[@id='selectable']/li[contains(@class,'ui-selected')]"));
 		System.out.println("Size after click/hod = " + selectedNumbers.size());
-		
+
 		for (WebElement number : selectedNumbers) {
 			System.out.println(number.getText());
 		}
-		
+
 		Assert.assertEquals(selectedNumbers.size(), 8);
 	}
-	
+
 //	@Test
 	public void TC_03_Click_And_Hold_Random() {
 		driver.get("https://jqueryui.com/resources/demos/selectable/display-grid.html");
-		
+
 		List<WebElement> numbers = driver.findElements(By.xpath("//ol[@id='selectable']/li"));
 		int numberSize = numbers.size();
 		System.out.println("Size before click/ hold = " + numberSize); // =12
-		
+
 		// |0 |1 |2 |3 |4 |5 |6 |...
 		// | element 01 | ...
-		
+
 		action.keyDown(Keys.CONTROL).perform();
-		
-		action.click(numbers.get(0))
-		.click(numbers.get(2))
-		.click(numbers.get(4))
-		.click(numbers.get(6))
-		.click(numbers.get(9))
-		.perform();
-		
-		action.keyUp(Keys.CONTROL).perform();	
-		
+
+		action.click(numbers.get(0)).click(numbers.get(2)).click(numbers.get(4)).click(numbers.get(6)).click(numbers.get(9)).perform();
+
+		action.keyUp(Keys.CONTROL).perform();
+
 		List<WebElement> selectedNumbers = driver.findElements(By.xpath("//ol[@id='selectable']/li[contains(@class,'ui-selected')]"));
 		System.out.println("Size after click/hod = " + selectedNumbers.size());
-		
+
 		for (WebElement number : selectedNumbers) {
 			System.out.println(number.getText());
 		}
-		
+
 		Assert.assertEquals(selectedNumbers.size(), 5);
 	}
 
@@ -123,7 +123,7 @@ public class Topic_09_User_Interactions {
 		action.doubleClick(driver.findElement(By.xpath("//button[text()='Double click me']"))).perform();
 		Thread.sleep(5000);
 		String demoText = driver.findElement(By.xpath("//p[@id='demo']")).getText();
-		
+
 		Assert.assertEquals(demoText, "Hello Automation Guys!");
 	}
 
@@ -133,46 +133,46 @@ public class Topic_09_User_Interactions {
 
 		action.contextClick(findByXpath("//span[text()='right click me']")).perform();
 		Thread.sleep(3000);
-		
+
 		action.moveToElement(findByXpath("//span[text()='Quit']")).perform();
 		Thread.sleep(3000);
-		
+
 		Assert.assertTrue(isElementDisplayed("//li[contains(@class,'context-menu-visible') and contains(@class,'context-menu-hover')]//span[text()='Quit']"));
-		
+
 		action.click(findByXpath("//span[text()='Quit']")).perform();
 		Thread.sleep(3000);
-		
+
 		Assert.assertEquals(driver.switchTo().alert().getText(), "clicked: quit");
-		
+
 		driver.switchTo().alert().accept();
 	}
-	
+
 	@Test
 	public void TC_06_Drag_And_Drop() throws InterruptedException {
 		driver.get("https://demos.telerik.com/kendo-ui/dragdrop/angular");
-		
+
 		WebElement sourceCircle = findByXpath("//div[@id='draggable']");
 		WebElement targetCircle = findByXpath("//div[@id='droptarget']");
-		
-		action.dragAndDrop(sourceCircle, targetCircle).perform();
+
+		action.dragAndDrop(sourceCircle, targetCircle).build().perform();
 //		Thread.sleep(3000);
-		
+
 		Assert.assertTrue(isElementDisplayed("//div[text()='You did great!']"));
 	}
-	
+
 //	@Test
 	public void TC_07_Drag_And_Drop_HTML5() throws InterruptedException, IOException {
 		driver.get("https://the-internet.herokuapp.com/drag_and_drop");
-		
+
 		String sourceCss = "#column-a";
 		String targetCss = "#column-b";
-		
+
 		String java_script = readFile(javascriptPath);
-		
+
 		// Inject Jquery lib to site
 //		String jquerscript = readFile(jqueryPath);
 //		javascriptExecutor.executeScript(jquerscript);
-		
+
 		// A to B
 		java_script = java_script + "$(\"" + sourceCss + "\").simulateDragDrop({ dropTarget: \"" + targetCss + "\"});";
 		javascriptExecutor.executeScript(java_script);
@@ -184,15 +184,15 @@ public class Topic_09_User_Interactions {
 		Thread.sleep(3000);
 		Assert.assertTrue(isElementDisplayed("//div[@id='column-b']/header[text()='B']"));
 	}
-	
+
 	public WebElement findByXpath(String xpathLocator) {
 		return driver.findElement(By.xpath(xpathLocator));
 	}
-	
+
 	public boolean isElementDisplayed(String xpathLocator) {
 		return findByXpath(xpathLocator).isDisplayed();
 	}
-	
+
 	public String readFile(String filePath) throws IOException {
 		Charset cs = Charset.forName("UTF-8");
 		FileInputStream stream = new FileInputStream(filePath);
@@ -209,7 +209,71 @@ public class Topic_09_User_Interactions {
 			stream.close();
 		}
 	}
-	
+
+//	@Test
+	public void TC_08_DragDrop_HTML5_Offset() throws InterruptedException, IOException, AWTException {
+		driver.get("http://the-internet.herokuapp.com/drag_and_drop");
+
+		String sourceXpath = "//div[@id='column-a']";
+		String targetXpath = "//div[@id='column-b']";
+
+		drag_the_and_drop_html5_by_xpath(sourceXpath, targetXpath);
+		Thread.sleep(3000);
+
+		drag_the_and_drop_html5_by_xpath(sourceXpath, targetXpath);
+		Thread.sleep(3000);
+
+		drag_the_and_drop_html5_by_xpath(sourceXpath, targetXpath);
+		Thread.sleep(3000);
+	}
+
+	public void drag_the_and_drop_html5_by_xpath(String sourceLocator, String targetLocator) throws AWTException {
+
+		WebElement source = driver.findElement(By.xpath(sourceLocator));
+		WebElement target = driver.findElement(By.xpath(targetLocator));
+
+		// Setup robot
+		Robot robot = new Robot();
+		robot.setAutoDelay(500);
+
+		// Get size of elements
+		Dimension sourceSize = source.getSize();
+		Dimension targetSize = target.getSize();
+
+		// Get center distance
+		int xCentreSource = sourceSize.width / 2;
+		int yCentreSource = sourceSize.height / 2;
+		int xCentreTarget = targetSize.width / 2;
+		int yCentreTarget = targetSize.height / 2;
+
+		Point sourceLocation = source.getLocation();
+		Point targetLocation = target.getLocation();
+		System.out.println(sourceLocation.toString());
+		System.out.println(targetLocation.toString());
+
+		// Make Mouse coordinate center of element
+		sourceLocation.x += 20 + xCentreSource;
+		sourceLocation.y += 110 + yCentreSource;
+		targetLocation.x += 20 + xCentreTarget;
+		targetLocation.y += 110 + yCentreTarget;
+
+		System.out.println(sourceLocation.toString());
+		System.out.println(targetLocation.toString());
+
+		// Move mouse to drag from location
+		robot.mouseMove(sourceLocation.x, sourceLocation.y);
+
+		// Click and drag
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.mouseMove(((sourceLocation.x - targetLocation.x) / 2) + targetLocation.x, ((sourceLocation.y - targetLocation.y) / 2) + targetLocation.y);
+
+		// Move to final position
+		robot.mouseMove(targetLocation.x, targetLocation.y);
+
+		// Drop
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
---
+++ selenium/webdriver_api/Topic_11_Window_Tab.java
@@ -0,0 +1,170 @@
+package webdriver_api;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_11_Window_Tab {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", ".\\libraries\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+//	@Test
+	public void TC_01_Windows() throws InterruptedException {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		
+		// ID cuả tan đang đứng
+		String parentID = driver.getWindowHandle();
+		System.out.println("Parent ID = " + parentID);
+		
+		driver.findElement(By.xpath("//a[text()='GOOGLE']")).click();
+		
+		// Tất cả ID của tất cả các tab
+//		Set<String> allIDs = driver.getWindowHandles();
+		// List <String> -> nó cho phép lưu dữ liệu trùng nhau
+		// Set <String> -> ko được phép trùng - nếu trùng thì chỉ lấy 1
+//		for(String id : allIDs) {
+//			System.out.println("ID = " + id);
+//		}
+		Thread.sleep(2000);
+		switchToWindowByTitle("Google");
+		Assert.assertEquals(driver.getTitle(), "Google");
+		
+		switchToWindowByTitle("SELENIUM WEBDRIVER FORM DEMO");
+		Thread.sleep(2000);
+		Assert.assertEquals(driver.getTitle(), "SELENIUM WEBDRIVER FORM DEMO");
+		
+		driver.findElement(By.xpath("//a[text()='FACEBOOK']")).click();
+		Thread.sleep(2000);
+		
+		switchToWindowByTitle("Facebook - Đăng nhập hoặc đăng ký");
+		Assert.assertEquals(driver.getTitle(), "Facebook - Đăng nhập hoặc đăng ký");
+		
+		closeAllWindowsWithoutParent(parentID);
+	}
+
+//	@Test
+	public void TC_02_Windows() throws InterruptedException {
+		driver.get("http://live.demoguru99.com/index.php");
+		
+		driver.findElement(By.xpath("//a[text()='Mobile']")).click();
+		
+		driver.findElement(By.xpath("//a[text()='Sony Xperia']//parent::h2//following-sibling::div[@class='actions']//a[text()='Add to Compare']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//span[contains(text(),' to comparis')]")).isDisplayed());
+		
+		driver.findElement(By.xpath("//button[@title='Compare']")).click();
+		Thread.sleep(2000);
+		
+		switchToWindowByTitle("Products Comparison List - Magento Commerce");		
+		Assert.assertTrue(driver.findElement(By.xpath("//img[@alt='Sony Xperia']")).isDisplayed());
+		
+		driver.findElement(By.xpath("//button[@class='button']")).click();
+		Thread.sleep(2000);
+		
+		switchToWindowByTitle("Mobile");
+		
+		driver.findElement(By.xpath("//a[text()='Clear All']")).click();
+		Thread.sleep(2000);
+		
+		driver.switchTo().alert().accept();
+		Assert.assertTrue(driver.findElement(By.xpath("//span[contains(text(),'cleared')]")).isDisplayed());
+	}
+	
+	@Test
+	public void TC_03_Windows() throws InterruptedException {
+		driver.get("https://kyna.vn/");	
+		String parentID = driver.getWindowHandle();
+		Thread.sleep(5000);
+		
+		System.out.println("Step 02 - Count popup number");
+		List <WebElement> fancyPopup = driver.findElements(By.xpath("//div[@class='fancybox-inner']"));
+		System.out.println("Fancy popup number = " + fancyPopup.size());
+		
+		if(fancyPopup.size() > 0) {
+			System.out.println("Step 03 - Check popup displayed and close it");
+			driver.findElement(By.cssSelector(".fancybox-close")).click();
+		}
+		
+		System.out.println("Step 04 - Click");		
+		driver.findElement(By.xpath("//img[@alt='facebook']")).click();
+		switchToWindowByTitle("Kyna.vn - Trang chủ | Facebook");
+		Assert.assertEquals(driver.getTitle(), "Kyna.vn - Trang chủ | Facebook");
+		
+		switchToWindowByTitle("Kyna.vn - Học online cùng chuyên gia");		
+		driver.findElement(By.xpath("//img[@alt='youtube']")).click();
+		switchToWindowByTitle("Kyna.vn - YouTube");
+		Assert.assertEquals(driver.getTitle(), "Kyna.vn - YouTube");
+		
+//		switchToWindowByTitle("Kyna.vn - Học online cùng chuyên gia");
+//		driver.findElement(By.xpath("//img[@alt='zalo']")).click();
+//		switchToWindowByTitle("Kyna.vn");
+//		Assert.assertEquals(driver.getTitle(), "‎Kyna.vn");
+		
+//		switchToWindowByTitle("Kyna.vn - Học online cùng chuyên gia");
+//		driver.findElement(By.xpath("//img[@alt='apple-app-icon']")).click();
+//		switchToWindowByTitle("‎KYNA on the App Store");
+//		Assert.assertEquals(driver.getTitle(), "‎KYNA on the App Store");
+		
+		switchToWindowByTitle("Kyna.vn - Học online cùng chuyên gia");
+		driver.findElement(By.xpath("//img[@alt='android-app-icon']")).click();
+		switchToWindowByTitle("‎KYNA - Học online cùng chuyên gia - Apps on Google Play");
+		Assert.assertEquals(driver.getTitle(), "‎KYNA - Học online cùng chuyên gia - Apps on Google Play");
+	
+		closeAllWindowsWithoutParent(parentID);
+	}
+
+	public void switchToChildWindowsByID(String parentID) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for(String runWindow : allWindows) {
+			if(!runWindow.equals(parentID)) {
+				driver.switchTo().window(runWindow);
+				break;
+			}
+		}
+	}
+	
+	public void switchToWindowByTitle(String title) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for(String runWindows : allWindows) {
+			driver.switchTo().window(runWindows);
+			String currentWin = driver.getTitle();
+			if(currentWin.equals(title)) {
+				break;
+			}
+		}
+	}
+	
+	public void closeAllWindowsWithoutParent(String parentWindow) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for(String runWindows : allWindows) {
+			if(!runWindows.equals(parentWindow)) {
+				driver.switchTo().window(runWindows);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(parentWindow);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
